2db92d643914de6d71038d8bb6bdf96cee0f6ac0
Mark Silva <mark.allen.silva@gmail.com>
2022-08-26 15:13:30 -0700

    complete

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 
-
-// create a variable for the count and set it to 0
-// listen for button clicks
-// if addPerson is pushed count++ and if removePerson is pushed count-- 
-// the counter on the page is incremented or decremented
+let countEl = document.getElementById("count-el");
 
 let count = 0;
 
 function increment () {
   count++;
-  document.getElementById("count").innerText = count;
+  countEl.innerText = count;
 }
 
 function decrement () {
   count--;
-  document.getElementById("count").innerText = count;
+  countEl.innerText = count;
 }
-
